c76728f8df7360ea52395e8de7550e8226403091
César Pérez <ca.andradeperez@gmail.com>
2026-02-03 21:27:58 +0000

    Finish celery introduction

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -60,6 +60,9 @@ class Dev(Configuration):
     CSRF_COOKIE_SAMESITE = "None"
     SESSION_COOKIE_SAMESITE = "None"
 
+    CELERY_RESULT_BACKEND = "django-db"
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
+
 
     # Application definition
 
@@ -72,6 +75,7 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         "movies",
         "gh",
+        "django_celery_results",
     ]
 
     MIDDLEWARE = [
